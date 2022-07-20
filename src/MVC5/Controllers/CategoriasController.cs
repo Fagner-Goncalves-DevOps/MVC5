deff6dccea167daa3cc0d4c4690cf6469bd8f521
fagner gonçalves <fagnergoncalves15@gmail.com>
2022-07-20 20:17:31 -0300

    MVCZÃO

---
+++ src/MVC5/Controllers/CategoriasController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC5.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(categorias);
+        }
+
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria()
+            {
+                CategoriaId = 1,
+                Nome = "NoteBooks"
+            },
+            new Categoria()
+            {
+                CategoriaId = 2,
+                Nome ="Monitores"
+            },
+            new Categoria()
+            {
+                CategoriaId = 3,
+                Nome = "Impressoras"
+            },
+            new Categoria()
+            {
+                CategoriaId = 4,
+                Nome = "Mouses"
+            },
+            new Categoria()
+            {
+                CategoriaId = 5,
+                Nome = "Desktops"
+            }
+        };
+
+    }
+}
+
---
+++ src/MVC5/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC5.Models
+{
+    public class Categoria
+    {
+        public long CategoriaId { get; set; }
+        public string Nome { get; set; }
+
+    }
+}
